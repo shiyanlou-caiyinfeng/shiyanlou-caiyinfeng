ada13580de78f752121964be2600c3a96af5cf83
841313643 <841313643@qq.com>
2017-11-26 22:41:14 +0800

    remove

--- news/app.py
+++ news/app.py
@@ -9,25 +9,32 @@ app.config['TEMPLATES_AUTO_RELOAD'] = True
 JSON_DIR_PATH='/home/shiyanlou/files'
 class Config(object):
     def __init__(self):
-        self._config={}
-    def set_config(self,filename):
-        with open(os.path.join(JSON_DIR_PATH,filename)) as file:
-            self._config=json.loads(file.read())
-    def get_config(self,value):
-        return self._config[value]
-    def get_json_config(self):
-        return self._config
+        self._config=self.set_config()
+    def set_config(self):
+        result={}
+        filenames=os.listdir(JSON_DIR_PATH)
+        for filename1 in filenames:
+            with open(os.path.join(JSON_DIR_PATH,filename1)) as file:
+                filename=filename1.split('.')[0]
+                result[filename]=json.loads(file.read())
+        return result
+    def get_title_config(self):
+        titles={}
+        for name,item in self._config.items():
+            titles[name]=item.get('title')
+        return titles
+    def get_json_config(self,filename):
+        return self._config.get(filename)
 test=Config()
 @app.errorhandler(404)
 def not_found(error):
     return render_template('404.html'),404
 @app.route('/')
 def index():
-    return render_template('index.html',)
+    return render_template('index.html',titles=test.get_title_config())
 @app.route('/files/<filename>')
 def file(filename):
-    test.set_connfig(filename)
-    news=get_json_config
+    news=test.get_json_config(filename)
     if not filename:
         abort(404)
     return render_template('file.html',news=news)
